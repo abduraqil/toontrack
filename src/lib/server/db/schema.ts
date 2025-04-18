b93e5d5a04439b55705114927e24d933e20ce1dc
abduraqil <aqilabdur@gmail.com>
2025-04-17 20:45:16 -0400

    created user schema starting

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,8 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar, text, integer } from 'drizzle-orm/pg-core';
+import { passive } from 'svelte/legacy';
 
 export const user = pgTable('user', {
 	id: serial('id').primaryKey(),
-	age: integer('age')
+	username: varchar({ length: 255 }).notNull(),
+	password: text().notNull(),
 });
