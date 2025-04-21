28178ae335656e0ee6473ffaa7321059163953e0
abduraqil <aqilabdur@gmail.com>
2025-04-20 20:55:39 -0400

    import action

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { enhance } from "$app/forms";
-    import type {ActionData} from "./+page.server";
+    import type {ActionData} from "./$types";
+    
 
     export let form: ActionData | undefined;
     
