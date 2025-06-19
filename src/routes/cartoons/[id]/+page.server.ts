4eadebb4deaa2ebf497c31d67b1814e9bf59576d
abduraqil <aqilabdur@gmail.com>
2025-06-19 05:03:42 -0400

    basic cartoon routing

---
+++ src/routes/cartoons/[id]/+page.server.ts
@@ -0,0 +1,29 @@
+import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+import { cartoons } from '$lib/server/db/schema';
+import { parse } from 'path';
+
+export const load: PageServerLoad = async ({ params }) => {
+    const { id } = params;
+
+    const cartoonID = parseInt(id, 10);
+
+    try {
+        const cartoon = await db.query.cartoons.findFirst({
+            where: eq(cartoons.id, cartoonID)
+        });
+
+        if (!cartoon) {
+            throw error(404, 'Cartoon not found');
+        }
+
+        return {
+            cartoon,
+        };
+    } catch (err) {
+        console.error('Error fetching cartoon:', err);
+        throw error(500, 'Failed to load cartoon');
+    }
+};
---
+++ src/routes/cartoons/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    export let data: PageData;
+    
+     $: cartoon = data.cartoon;
+
+</script>
+
+<svelte class="head">
+
+</svelte>
+
+<div>
+    <h1>{cartoon.name}</h1>
+</div>
