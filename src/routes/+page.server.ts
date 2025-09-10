e2cb7ad66b8e71501e42d8a2a5948f98a91afede
Abdur Rehman Aqil <aqilabdur@gmail.com>
2025-09-10 16:45:39 -0400

    commented out for now

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,11 @@
+// import { db } from '$lib/server/db'
+// import type { PageServerLoad } from './$types'
+
+// export const load: PageServerLoad = async ({ locals }) => {
+	
+// 	try {
+// 		const cartoons = await db.query.cartoons.find
+
+// 	}
+
+// }
