2bc010c8a8598855b968e700e0fdd5594071da65
Abdur Rehman Aqil <aqilabdur@gmail.com>
2025-09-11 19:43:15 -0400

    empty button components

---
+++ src/lib/components/friend.svelte
@@ -0,0 +1,11 @@
+<button
+	class="
+		px-4 py-2
+		btn btn-success
+		text-base-100
+		rounded-md
+		hover:bg-success-focus
+		transition-colors text-sm font-medium
+		">
+	Friend
+</button>
---
+++ src/lib/components/message.svelte
@@ -0,0 +1,11 @@
+<button
+	class="
+		px-4 py-2
+		btn btn-info
+		text-base-100
+		rounded-md
+		hover:bg-info-focus
+		transition-colors text-sm font-medium
+		">
+	Message
+</button>
