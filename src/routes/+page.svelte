28f6d3c902d1bd1fc678a0eca7e793ba6022db1a
Abdur Rehman Aqil <aqilabdur@gmail.com>
2025-09-11 19:43:24 -0400

    simple home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
-<a href="/signup">signup</a>
-<a href="/login">login</a>
-<a href="/">test</a>
-<div>
-    <main>
-        <p>test</p>
-    </main>
-</div>
+<!-- TODO
+ -[] When signed in, sign up leads you to the fake home page
+ -[]
+-->
+<script lang="ts">
+    import '../app.css'
+
+    // const StaffPicks = [
+    //     {}
+    // ]
+
+</script>
+
+<main>
+    <div class="hero bg-base-100 rounded-lg p-8">
+        <div class="hero-content flex-col text-center">
+            <h1 class="text-4xl font-bold text-primary mb-4">Track Your Favorite Cartoons</h1>
+            <p class="text-lg text-base-content">
+                Discover, rate, and review animated content from your classic childhood 
+                on Cartoon Network to modern streaming shows
+            </p>
+            <div>
+                <a class="btn btn-lg btn-primary" href="/signup" aria-label="Sign Up">
+                    Get Started
+                </a>
+            </div>
+
+            <!-- Staff Picks-->
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</main>
